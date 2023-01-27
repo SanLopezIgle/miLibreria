45b3ecb30af192c3760432364e15ead58e8730f6
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-01-27 08:49:39 +0100

    entrada

--- src/EntradaSalida.java
+++ src/EntradaSalida.java
@@ -1,44 +1,44 @@
+import com.sun.source.tree.TryTree;
+
 import javax.swing.*;
 import java.lang.reflect.Parameter;
 import java.util.Scanner;
 
 public class EntradaSalida {
-
-    public static int dameEntero(){
-        int entero = Integer.parseInt(JOptionPane.showInputDialog("Dame un entero: "));
-
-        do {
-            if (entero == (int) entero) return entero;
-            else entero = Integer.parseInt(JOptionPane.showInputDialog("No es un entero.\nDame otro valor: "));
-        }while (entero != (int)entero);
-        return entero;
-    }
-
     static final int OPCION_ENTERO = 1;
     static final int OPCION_CADENA = 2;
+    static final int SALIDA_CONSOLA = 1;
+    static final int SALIDA_VENTANA = 2;
 
-
+    /**
+     * Entrada por consola de un entero o una cadena
+     * @param opcion entrada de entero (OPCION_ENTERO) o entrada de cadena (OPCION_CADENA)
+     * @return true si correcto y si no false
+     */
     public static boolean entrada(int opcion){
         Scanner sc = new Scanner(System.in);
         switch(opcion){
-            case 1:
-                System.out.println("Dame un entero: ");
-                sc.nextInt();
-                return true;
-            case 2:
-                System.out.println("Dame uan cadena: ");
-                sc.nextLine();
-                return true;
+            case OPCION_ENTERO:
+                try {
+                    int entero = 0;
+                    System.out.println("Dame un entero: ");
+                    sc.nextInt();
+                    return true;
+                }catch (Exception e){
+                    return false;
+                }
+            case OPCION_CADENA:
+                try {
+                    System.out.println("Dame una cadena: ");
+                    sc.nextLine();
+                    return true;
+                }catch (Exception e){
+                    return false;
+                }
             default:
                 return false;
         }
-
-
-
     }
-
-    static final int SALIDA_CONSOLA = 1;
-    static final int SALIDA_VENTANA = 2;
     /**
      * Salida por ventana o consola de un mensaje
      * @param mensaje cadena que queremos imprimir
@@ -49,11 +49,20 @@ public class EntradaSalida {
         switch (dispositivo) {
             //TODO: try catch
             case SALIDA_CONSOLA:
-                System.out.println("Consola: " + mensaje);
-                return true;
+                try {
+                    System.out.println("Consola: " + mensaje);
+                    return true;
+                } catch (Exception e){
+                    return false;
+                }
             case SALIDA_VENTANA:
-                JOptionPane.showMessageDialog(null, mensaje);
-                return true;
+                try {
+                    JOptionPane.showMessageDialog(null, mensaje);
+                    JOptionPane.showMessageDialog(null, "Sale por ventana" + mensaje.toCharArray()[20]);
+                    return true;
+                }catch (Exception e){
+                    return false;
+                }
             default:
                 return false;
         }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+/*
+        Boolean isOK_ventana = false;
+        Boolean isOK_consola = false;
 
-        Boolean resultado = false;
+        isOK_consola = EntradaSalida.salida("Hola mundo <3", EntradaSalida.SALIDA_CONSOLA);
+        if(!isOK_consola) System.out.println("ERROR CONSOLA");
+        isOK_ventana = EntradaSalida.salida("Hola mundo <3", EntradaSalida.SALIDA_VENTANA);
+        if(!isOK_ventana) System.out.println("ERROR VENTANA");
+*/
+        Boolean isOK_entero = false;
+        Boolean isOK_cadena = false;
 
-        resultado = EntradaSalida.salida("Hola mundo <3", EntradaSalida.SALIDA_CONSOLA);
-        resultado = EntradaSalida.salida("Hola mundo <3", EntradaSalida.SALIDA_VENTANA);
-
-        resultado = EntradaSalida.entrada(EntradaSalida.OPCION_ENTERO);
-        resultado = EntradaSalida.entrada(EntradaSalida.OPCION_CADENA);
+        isOK_entero = EntradaSalida.entrada(EntradaSalida.OPCION_ENTERO);
+        if(!isOK_entero) System.out.println("ERROR ENTERO");
+        isOK_cadena = EntradaSalida.entrada(EntradaSalida.OPCION_CADENA);
     }
 }
